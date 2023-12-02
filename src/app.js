cd0bfbedd5c035ac47ad3c2d07c4ae0b499de53d
NyawiraGitaka <nyawira.gitaka@gmail.com>
2023-12-02 19:47:53 +0000

    Adding the search functionality to display data using API

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
-function searchCity(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-input");
-    let cityName = document.querySelector(".weather-city");
-    cityName.innerHTML = searchInput.value;
-};
+function updateWeatherDetails(response) {
+  let cityTemp = document.querySelector("#weather-temp-value");
+  let cityName = document.querySelector(".weather-city");
+
+  cityTemp.innerHTML = Math.round(response.data.temperature.current);
+  cityName.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "314o3fa79375c2341f0ct1d3593ab8a2";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeatherDetails);
+}
+
+function getCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+
+  searchCity(searchInput.value);
+}
 
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", searchCity);
+searchForm.addEventListener("submit", getCity);
+searchCity("Tallinn")
